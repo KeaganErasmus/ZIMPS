f29791975f78616de7d0e0ae8e22d631c35a50f0
KeaganErasmus <keaganerasmus12@gmail.com>
2023-08-04 11:11:43 +1200

    more doddgy code still trying to understand oop

--- console.py
+++ console.py
@@ -2,29 +2,26 @@ class console:
     """
     Console that displays text and allows for user input
     """
-    def __init__(self, args: []) -> None:
-        self.args = args
 
-        while self.user_input() != "quit":
-            self.user_input()
-    
+    def __init__(self, args: list[str]) -> None:
+        self.args = args
 
     def print_commands(self):
         """
         Print the commands that can be used 
         """
-        print(self.args)
+        print(f'These are the commands that you can use: \n{self.args}')
 
-
-    def user_input(self):
+    def user_input(self) -> str:
         """
         Takes the raw input from a user and displays the message
         """
-        user_input = input("To see all commands type help: ").strip().lower()
+        user_input: str = input("To see all commands type help: ").strip().lower()
 
-        if user_input == "help":
-            self.print_commands()
-        if user_input == "quit":
-            return "quit"
-        
         return user_input
+
+    def run_commands(self):
+        if self.user_input() == "help":
+            self.print_commands()
+        else:
+            print("That is not a recognized command: Type help to see all commands")
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from GUI.gui import GUI
 from console import console
 
+
 def main():
     # app = GUI()
     print("Welcome to Zombies in my pocket")
-    view = console(["help", "start", "stop"])
+    view = console(["help", "start", "stop", "quit"])
+
+    while view.user_input() != "quit":
+        view.run_commands()
 
 
 if __name__ == "__main__":
