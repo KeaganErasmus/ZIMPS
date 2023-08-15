c5e7c17296372a115ceaadbb8c2865ccf894f29d
sat0791 <sat0791@arastudent.ac.nz>
2023-08-16 11:56:15 +1200

    updat main

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from GUI.gui import GUI
 from console import Console
 from pickling import Pickling
+from cmdconsole import CMDConsole
 
 
 def main():
     # app = GUI()
     print("Welcome to Zombies in my pocket")
-    view = Console("help", "start", "stop", "quit")
+    vcon = Console("help", "start", "stop", "quit")
+    vcmd = CMDConsole()
 
-    while view.userinput != "quit":
-        view.user_input()
-        view.run_commands()
+    vcmd.cmdloop()
+
+    while vcon.userinput != "quit":
+        vcon.user_input()
+        vcon.run_commands()
 
 
 if __name__ == "__main__":
