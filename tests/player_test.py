d8252bd0dbd6c3595ec1526c81931bd2a84dcff7
Keagan Erasmus <Keagan.Erasmus@alliedtelesis.co.nz>
2023-08-22 15:27:00 +1200

    renaming the test class to be more descriptive

--- tests/player_test.py
+++ tests/player_test.py
@@ -2,11 +2,12 @@ import unittest
 from game import Game
 
 the_game = Game()
-class player_movement_test(unittest.TestCase):
+class player_tests(unittest.TestCase):
     def test_player_start_location(self):
         player_loc = the_game.player.location
-
         self.assertEqual(player_loc == (5, 3), True)  # add assertion here
+
+    def
 
 
 if __name__ == '__main__':
