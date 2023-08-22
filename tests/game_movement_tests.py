98cf6c2389b1c74252a6fea7c3df86b3faa3ba93
Keagan Erasmus <Keagan.Erasmus@alliedtelesis.co.nz>
2023-08-22 16:03:45 +1200

    added game tests

---
+++ tests/game_movement_tests.py
@@ -0,0 +1,25 @@
+import unittest
+from game import Game
+
+the_game = Game()
+
+class Test_player_moving(unittest.TestCase):
+    def test_player_moving_n_from_foyer(self):
+        # Player starting location is (x=5, y=3)
+        # moving up takes away from x
+        # eg (x=4, y=3)
+        the_game.player_turn('n')
+        print(the_game.player.location)
+        self.assertEqual(the_game.player.location, (4, 3))
+
+
+    def test_player_moving_s_from_foyer(self):
+        # Player starts in the foyer
+        # The foyer has 1 exit to the north
+        # So player should not be able to move any other direction
+        the_game.player_turn('s')
+        self.assertEqual(the_game.player.location, (5, 3))  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
