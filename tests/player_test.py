5cc1ea43fa470af07874984584a6e7d473b25062
Keagan Erasmus <Keagan.Erasmus@alliedtelesis.co.nz>
2023-08-22 15:29:24 +1200

    renaming the test class to be more descriptive, and added some more cases

--- tests/player_test.py
+++ tests/player_test.py
@@ -2,12 +2,14 @@ import unittest
 from game import Game
 
 the_game = Game()
-class player_tests(unittest.TestCase):
+class player_initial_stats(unittest.TestCase):
     def test_player_start_location(self):
         player_loc = the_game.player.location
-        self.assertEqual(player_loc == (5, 3), True)  # add assertion here
+        self.assertEqual(player_loc, (5, 3))  # add assertion here
 
-    def
+    def test_player_starting_health(self):
+        player_health = the_game.player.health
+        self.assertEqual(player_health, 6)
 
 
 if __name__ == '__main__':
