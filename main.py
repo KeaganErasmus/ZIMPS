bb3eb15344e9506de2250895e816d3b423d1c3e9
keaganerasus <keaganerasmus12@gmail.com>
2023-08-27 14:56:14 +1200

    added the board_size as a input parameter

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ def main():
                         help='The start x-coordinate for the player.')
     parser.add_argument('--y', type=int, default=3,
                         help='The start y-coordinate for the player.')
+    
+    parser.add_argument('--rows', type=int, default=7,
+                        help="The number of rows on the Board")
+    parser.add_argument('--cols', type=int, default=7,
+                    help="The number of cols on the Board")   
     args = parser.parse_args()
 
     print("Welcome to Zombies in my pocket")
     start_coordinates = (args.x, args.y)
-    run = Console(start_coordinates)
+    board_size = (args.rows, args.cols)
+    
+    run = Console(start_coordinates, board_size)
     run.cmdloop()
     run.game.gui.root.mainloop()
 
