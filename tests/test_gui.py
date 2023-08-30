900ecf44dba259fb9b703ac371cddab5a1404ca5
hcdiekmann <hcdiekmann@hotmail.com>
2023-08-30 14:19:07 +1200

    pep8 formated

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -20,15 +20,19 @@ class TestGUI(unittest.TestCase):
     def test_update_dev_cards(self):
         self.gui.update_dev_cards(cards_count=5, current_time="11 PM")
         self.assertEqual(self.gui.label_time.cget("text"), "Time: 11 PM")
-        self.assertEqual(self.gui.label_dev_cards.cget("text"), "Development Cards: 5")
+        self.assertEqual(self.gui.label_dev_cards.cget("text"),
+                         "Development Cards: 5")
 
     def test_update_tile_count(self):
         self.gui.update_tile_count(indoor_count=3, outdoor_count=4)
-        self.assertEqual(self.gui.lable_outdoor_tiles.cget("text"), "Outdoor Tiles: 4")
-        self.assertEqual(self.gui.lable_indoor_tiles.cget("text"), "Indoor Tiles: 3")
-        
+        self.assertEqual(self.gui.lable_outdoor_tiles.cget("text"),
+                         "Outdoor Tiles: 4")
+        self.assertEqual(self.gui.lable_indoor_tiles.cget("text"),
+                         "Indoor Tiles: 3")
+
     def test_update_player_info(self):
-        self.gui.update_player_info(health=5, attack=2, items=["Chainsaw"], location=(1, 2))
+        self.gui.update_player_info(health=5, attack=2, items=["Chainsaw"],
+                                    location=(1, 2))
         self.assertEqual(self.gui.lable_health.cget("text"), "Health: 5")
         self.assertEqual(self.gui.lable_attack.cget("text"), "Attack: 2")
         self.assertEqual(self.gui.items.cget("text"), "Items: ['Chainsaw']")
@@ -39,12 +43,12 @@ class TestGUI(unittest.TestCase):
         self.gui.move_player(1, 2)
         self.assertEqual(self.gui.player_row, 1)
         self.assertEqual(self.gui.player_col, 2)
-        
+
     def test_player_marker_color(self):
         self.gui.draw_player()
         marker_color = self.gui.canvas.itemcget(self.gui.player_marker, "fill")
         self.assertEqual(marker_color, "red")
-        
+
 
 if __name__ == '__main__':
     unittest.main()
